17b71fa74eb0c51ef2a29c12e60aa40496d10d6c
vyacheslav <dev.vyacheslav@gmail.com>
2018-02-04 15:39:22 +0300

    install telegram bot+ test case

---
+++ index.js
@@ -0,0 +1,11 @@
+const tBot = require('node-telegram-bot-api');
+
+const TOKEN = '507958990:AAG-p27Ciz9Y4HjWmudFDBhrUsC4PHtHO0A';
+
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, `hello from heroku, bot says : "hi, ${msg.from.first_name}"`);
+
+})
